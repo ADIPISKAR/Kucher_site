53342c4bd8e88922f7d34b5e1e9e1748390eb006
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-11-07 15:08:19 +0300

    1

--- src/resources/views/page/tg_page.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.auth')
-
-@section('content')
-<div class="container py-3">
-    <div class="col-md-8">
-        <p class="text-white">Это страница для TG!</p>
-    </div>
-</div>
-@endsection
