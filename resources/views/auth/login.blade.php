cdfed5a5062b615c1ee197c456a471e230465f76
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-11-10 13:49:43 +0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
                 <p class="text-center Second-Text">Теперь работать с нами стало ещё удобнее и современнее!</p>
             </div>
 
-                <form action="{{ route('login') }}" method="POST">
+                <form action="{{ secure_url('login') }}" method="POST">
                     @csrf
 
                     <div class="mb-3">
