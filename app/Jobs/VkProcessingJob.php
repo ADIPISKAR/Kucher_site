9e2557726886872e7cd921bbfeeb4b3874ddcde7
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-11-12 20:47:16 +0300

    Update VkProcessingJob.php

--- app/Jobs/VkProcessingJob.php
+++ app/Jobs/VkProcessingJob.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class VkProcessingJob implements ShouldQueue
+class VkProcessingJob implements Dispatchable
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
