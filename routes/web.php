ab18f2f71f43143bfdd471a66c9845281a7e71a3
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-11-12 18:46:45 +0300

    1

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,16 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/vk_page', [App\Http\Controllers\view\VkViewController::class, 'index'])->name('vk_page');
 Route::get('/tg_page', [App\Http\Controllers\view\TgViewController::class, 'index'])->name('tg_page');
 Route::get('/tg_settings', [App\Http\Controllers\view\SettingsViewController::class, 'index'])->name('setting_page');
-Route::get('/Add_list', [App\Http\Controllers\Back_doing\Controller_settings::class, 'index'])->name('Controller_settings');
+
+Route::get('/Add_list', [App\Http\Controllers\Back_doing\Controller_settings::class, 'index'])->name('Add_list');
+
 Route::post('/Add_user', [App\Http\Controllers\Back_doing\Controller_settings::class, 'New_user'])->name('New_user');
 Route::post('/Add_message', [App\Http\Controllers\Back_doing\Controller_settings::class, 'add_message'])->name('add_message');
 Route::delete('/account/{id}', [App\Http\Controllers\Back_doing\Controller_settings::class, 'destroy_user'])->name('destroy_user');
 Route::delete('/message/{id}', [App\Http\Controllers\Back_doing\Controller_settings::class, 'message_destroy'])->name('message_destroy');
+
 Route::get('/add_message_route', [App\Http\Controllers\Back_doing\Controller_settings::class, 'add_message_route'])->name('add_message_route');
+
 Route::post('/vk_doing', [App\Http\Controllers\Back_doing\VkController::class, 'init'])->name('vk_doing');
 
 
