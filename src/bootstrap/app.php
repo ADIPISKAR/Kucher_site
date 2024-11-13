d223ce9ec0a92b206f6aa58ac8d7d65adc04a7d8
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-11-13 23:36:59 +0300

    Update app.php

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -1,31 +1,22 @@
-<?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Routing\Router;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Foundation\Configuration\Exceptions;
 
-// Создание нового приложения
 $app = new Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
-// Настройка маршрутов
-$app->withRouting(function (Router $router) {
-    $router->group(['namespace' => 'App\Http\Controllers'], function () {
-        require __DIR__.'/../routes/web.php';
-        require __DIR__.'/../routes/console.php';
-    });
-});
+// Подключаем маршруты
+require __DIR__.'/../routes/web.php';
+require __DIR__.'/../routes/console.php';
 
-// Настройка промежуточного ПО
-$app->withMiddleware(function (Middleware $middleware) {
-    // Ваше промежуточное ПО
-});
-
-// Обработка исключений
-$app->withExceptions(function (Exceptions $exceptions) {
-    // Ваши обработчики исключений
+// Можно также настроить группу маршрутов, если нужно
+$app->router->group([
+    'namespace' => 'App\Http\Controllers',
+], function ($router) {
+    require __DIR__.'/../routes/web.php';
+    require __DIR__.'/../routes/console.php';
 });
 
 return $app;
