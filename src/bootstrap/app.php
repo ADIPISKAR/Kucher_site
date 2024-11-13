6dc846a3106efc21d3dd461356c19dc2158a1a33
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-11-13 23:34:43 +0300

    1

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -1,18 +1,31 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Routing\Router;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Configuration\Exceptions;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+// Создание нового приложения
+$app = new Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+);
+
+// Настройка маршрутов
+$app->withRouting(function (Router $router) {
+    $router->group(['namespace' => 'App\Http\Controllers'], function () {
+        require __DIR__.'/../routes/web.php';
+        require __DIR__.'/../routes/console.php';
+    });
+});
+
+// Настройка промежуточного ПО
+$app->withMiddleware(function (Middleware $middleware) {
+    // Ваше промежуточное ПО
+});
+
+// Обработка исключений
+$app->withExceptions(function (Exceptions $exceptions) {
+    // Ваши обработчики исключений
+});
+
+return $app;
