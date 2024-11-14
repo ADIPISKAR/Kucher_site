16bf2ac0ebf4304d9b9f235dd0bdc064f46d4c9a
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-11-14 13:18:40 +0300

    Update VkController.php

--- src/app/Http/Controllers/Back_doing/VkController.php
+++ src/app/Http/Controllers/Back_doing/VkController.php
@@ -7,6 +7,7 @@ use GuzzleHttp\Client;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\API\VkLongPollService;
 use App\Jobs\VkProcessingJob;
+use Illuminate\Bus\Queueable;
 
 use App\Models\AccountListing; // Убедитесь, что добавили этот импорт
 use App\Models\Message; // Убедитесь, что добавили этот импорт
