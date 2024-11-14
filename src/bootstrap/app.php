a1478ff7d4b95b822d27af3c01e299d3d8b2bf7a
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-11-14 11:16:16 +0300

    1

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -1,22 +1,18 @@
+<?php
+
 use Illuminate\Foundation\Application;
-use Illuminate\Routing\Router;
-use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
-$app = new Application(
-    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
-);
-
-// Подключаем маршруты
-require __DIR__.'/../routes/web.php';
-require __DIR__.'/../routes/console.php';
-
-// Можно также настроить группу маршрутов, если нужно
-$app->router->group([
-    'namespace' => 'App\Http\Controllers',
-], function ($router) {
-    require __DIR__.'/../routes/web.php';
-    require __DIR__.'/../routes/console.php';
-});
-
-return $app;
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
