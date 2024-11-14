6cdc5c547fd5794f4526bdf36ee9b7ee617a420d
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-11-14 15:00:19 +0300

    Update VkProcessingJob.php

--- src/app/Jobs/VkProcessingJob.php
+++ src/app/Jobs/VkProcessingJob.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Jobs;
+
 use App\Http\Controllers\API\VkApi;
 use GuzzleHttp\Client;
 use Illuminate\Bus\Queueable;
-
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
@@ -14,7 +14,7 @@ class VkProcessingJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, SerializesModels;
 
-    protected $access_token;   // Делаем свойства доступными
+    protected $access_token;
     protected $mess_pass;
     protected $messagesArray;
 
@@ -29,26 +29,30 @@ class VkProcessingJob implements ShouldQueue
     {
         echo 'Обработка началась';
         try {
-            // Создаем объект VKApi
-            
+            $VK = new VKApi;
     
-
-    
-            while (true){
-
-                $VK = new VKApi;
-                
+            while (true) {
+                // Получаем новое сообщение
                 $drop_message = $VK->getMessageLast($this->access_token);
                 $last_message = $drop_message['text'];
+                $from_id = $drop_message['from_id'];  // id отправителя для дополнительной логики
 
+                // Логирование последнего сообщения
+                \Log::info("Последнее сообщение: $last_message");
+
+                // Если встречаем сообщение о "слишком много лайков", останавливаем выполнение
                 if (strpos($last_message, 'Слишком много лайков за сегодня') !== false) {
+                    \Log::info("Цикл завершен из-за превышения лайков.");
                     return false;
                 }
         
                 $restartCycle = false;
 
+                // Если найдено ключевое слово в $this->mess_pass
                 foreach ($this->mess_pass as $iskl) {
                     if (strpos($last_message, $iskl) !== false) {
+                        // Ответы на основе найденного сообщения
+                        \Log::info("Сообщение найдено, начинаем действия.");
                         $VK->sendMessageWithGuzzle($this->access_token, '/start');
                         sleep(rand(2, 5));
                         $VK->sendMessageWithGuzzle($this->access_token, '1');
@@ -60,10 +64,12 @@ class VkProcessingJob implements ShouldQueue
                 }
 
                 if ($restartCycle) {
-                    continue; 
+                    // Если цикл перезапускается, продолжаем его
+                    continue;
                 }
         
-                if ((rand(0, 10) >= 5) && ($drop_message['from_id'] == '-91050183')) {
+                // Логика для других вариантов сообщений
+                if ((rand(0, 10) >= 5) && ($from_id == '-91050183')) {
                     sleep(rand(2, 5));
                     $VK->sendMessageWithGuzzle($this->access_token, '2');
                     sleep(rand(2, 5));
@@ -76,16 +82,16 @@ class VkProcessingJob implements ShouldQueue
                 }
 
                 if ($restartCycle) {
-                    continue; 
+                    // Если цикл перезапускается, продолжаем его
+                    continue;
                 }
 
-                sleep(3);
+                // Небольшая задержка перед следующим запросом
+                sleep(3);  // Задержка 3 секунды, чтобы избежать слишком частых запросов
             }
 
         } catch (\Exception $e) {
-            // Логируем ошибку
             \Log::error('Ошибка в обработке сообщения VK: ' . $e->getMessage());
         }
     }
-    
 }
