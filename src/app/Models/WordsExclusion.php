300d6c0a25a0d4c39da9914ef3148aa12c3c807a
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-11-19 16:55:04 +0300

    1

--- src/app/Models/WordsExclusion.php
+++ src/app/Models/WordsExclusion.php
@@ -9,12 +9,9 @@ class WordsExclusion extends Model
 {
     use HasFactory;
 
-    // Указываем таблицу, если имя модели не совпадает с названием таблицы в БД
     protected $table = 'words_exclusion';
 
-    // Если вы хотите разрешить массовое присваивание данных, указываем столбцы, которые можно заполнять
     protected $fillable = ['word'];
 
-    // Если не хотите использовать timestamp поля, можно отключить их
-    public $timestamps = true; // Убедитесь, что в вашей миграции есть `created_at` и `updated_at` поля
+    public $timestamps = true; 
 }
