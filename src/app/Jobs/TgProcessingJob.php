42d9e266eb1e7a70878be79e8ce1bd0ec0b61e15
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-11-25 12:08:39 +0300

    Update TgProcessingJob.php

--- src/app/Jobs/TgProcessingJob.php
+++ src/app/Jobs/TgProcessingJob.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use App\Http\Controllers\API\TgClient;
+use App\Models\WordsExclusion;
 
 class TgProcessingJob implements ShouldQueue
 {
