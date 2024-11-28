987394329911dbcd8cc79c8911ceb936e271eefb
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-11-28 22:17:34 +0300

    Update TgApi.php

--- src/app/Http/Controllers/API/TgApi.php
+++ src/app/Http/Controllers/API/TgApi.php
@@ -8,20 +8,13 @@ if (!file_exists('/var/www/html/Kucher_site/src/madeline.php')) {
 include '/var/www/html/Kucher_site/src/madeline.php';
 
 use danog\MadelineProto\API;
-use danog\MadelineProto\Settings\Bot;
+// use danog\MadelineProto\Settings\Bot;
 
 class TgApi {
     private $MadelineProto;
 
     public function __construct() {
-        $settings = new Bot([
-            'app_info' => [
-                'api_id' => '23309931', // Ваш API ID
-                'api_hash' => 'a1b55a9fa815fa90cf817b0390a430cf' // Ваш API hash
-            ]
-        ]);
-
-        $this->MadelineProto = new \danog\MadelineProto\API('/var/www/html/Kucher_site/src/session/session.madeline', $settings);
+        $this->MadelineProto = new \danog\MadelineProto\API('/var/www/html/Kucher_site/src/session/session.madeline');
         $this->MadelineProto->start();
 
         $me = $this->MadelineProto->getSelf();
