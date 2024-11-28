bb89b105260302aa57ce5ba83b187a92ff5c746d
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-11-28 22:04:40 +0300

    Update TgApi.php

--- src/app/Http/Controllers/API/TgApi.php
+++ src/app/Http/Controllers/API/TgApi.php
@@ -10,18 +10,21 @@ if (!file_exists('madeline.php')) {
 include 'madeline.php';
 
 use danog\MadelineProto\API;
+use danog\MadelineProto\Settings\AppInfo;
+use danog\MadelineProto\Settings\API as APISettings;
 
 class TgApi {
     private $MadelineProto;
 
     public function __construct() {
+        $appInfo = new AppInfo([
+            'api_id' => '23309931', // Ваш API ID
+            'api_hash' => 'a1b55a9fa815fa90cf817b0390a430cf' // Ваш API hash
+        ]);
 
-        $settings = [
-            'app_info' => [
-                'api_id' => '23309931', // Замените на ваш API ID
-                'api_hash' => 'a1b55a9fa815fa90cf817b0390a430cf' // Замените на ваш API hash
-            ]
-        ];
+        $settings = new APISettings([
+            'app_info' => $appInfo
+        ]);
 
         $this->MadelineProto = new \danog\MadelineProto\API('/var/www/html/Kucher_site/src/session/session.madeline', $settings);
         $this->MadelineProto->start();
