da20fce5cf4fe3d7360bd0629a6a484bc1502b0c
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-11-29 14:37:36 +0300

    Update TgApi.php

--- src/app/Http/Controllers/API/TgApi.php
+++ src/app/Http/Controllers/API/TgApi.php
@@ -14,7 +14,23 @@ class TgApi {
     private $MadelineProto;
 
     public function __construct() {
-        $this->MadelineProto = new \danog\MadelineProto\API('/var/www/html/Kucher_site/src/session/');
+
+        $settings = [
+            'app_info' => [
+                'api_id' => '23309931',  // Replace with your actual API ID
+                'api_hash' => 'a1b55a9fa815fa90cf817b0390a430cf',  // Replace with your actual API Hash
+            ],
+            'logger' => [
+                'logger' => 0, // 0 for no logging, 1 for logging to file, 2 for logging to console
+            ],
+            'serialization' => [
+                'serialization_interval' => 300, // Interval in seconds for automatic session serialization
+            ],
+            'updates' => [
+                'handle_updates' => false, // Set to true if you want to handle updates manually
+            ],
+        ];
+        $MadelineProto = new API('session.madeline', $settings);
         $this->MadelineProto->start();
 
         $me = $this->MadelineProto->getSelf();
