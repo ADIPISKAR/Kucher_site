3e82024ecc80c40630d63f7b9f9cce5e8e588715
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-12-02 08:43:28 +0300

    Update VkProcessingJob.php

--- src/app/Jobs/VkProcessingJob.php
+++ src/app/Jobs/VkProcessingJob.php
@@ -62,6 +62,7 @@ class VkProcessingJob implements ShouldQueue
                     }
                 }
     
+                
                 if ($skipCurrentIteration) {
                     continue;
                 }
