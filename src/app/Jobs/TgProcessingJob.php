5fbb0b36efcfbe5cc0016543be1c0dd2b3aac9e7
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-12-02 16:15:42 +0300

    Update TgProcessingJob.php

--- src/app/Jobs/TgProcessingJob.php
+++ src/app/Jobs/TgProcessingJob.php
@@ -28,8 +28,8 @@ class TgProcessingJob implements ShouldQueue
 
     public function __construct($apiId, $apiHash)
     {
-        $this->apiId = '23309931';
-        $this->apiHash = 'a1b55a9fa815fa90cf817b0390a430cf';
+        $this->apiId = $apiId;
+        $this->apiHash = $apiHash;
         $this->excludedWords = WordsExclusion::pluck('word')->filter()->toArray();
         $this->sessionFile = env('TELEGRAM_SESSION_FILE', 'session.madeline');
     }
@@ -40,8 +40,8 @@ class TgProcessingJob implements ShouldQueue
             // Создание объекта настроек для MadelineProto
             $settings = new Settings([
                 'app_info' => [
-                    'api_id' => $this->apiId,
-                    'api_hash' => $this->apiHash,
+                    'api_id' => '23309931',
+                    'api_hash' => 'a1b55a9fa815fa90cf817b0390a430cf',
                 ],
             ]);
 
