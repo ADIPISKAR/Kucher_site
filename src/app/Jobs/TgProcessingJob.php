5aeb0bf6070dc93117ed7d0e4c958059bcd2b736
Artem_256 <97385150+ADIPISKAR@users.noreply.github.com>
2024-12-04 13:03:33 +0300

    Update TgProcessingJob.php

--- src/app/Jobs/TgProcessingJob.php
+++ src/app/Jobs/TgProcessingJob.php
@@ -25,27 +25,27 @@ class TgProcessingJob implements ShouldQueue
     public function __construct()
     {
         $this->excludedWords = WordsExclusion::pluck('word')->filter()->toArray();
-        $this->sessionFile = env('TELEGRAM_SESSION_FILE', 'session.madeline');  // Указываем полный путь
+        $this->sessionFile = env('TELEGRAM_SESSION_FILE');  // Указываем полный путь
     }
 
     public function handle()
     {
         try {
-            // $settings = (new \danog\MadelineProto\Settings\AppInfo)
-            //     ->setApiId(23309931)
-            //     ->setApiHash('a1b55a9fa815fa90cf817b0390a430cf');
+            $settings = (new \danog\MadelineProto\Settings\AppInfo)
+                ->setApiId('23309931')
+                ->setApiHash('a1b55a9fa815fa90cf817b0390a430cf');
 
 
-            $settings = new Settings([
-                'app_info' => [
-                    'api_id' => '23309931',  // Ваш api_id
-                    'api_hash' => 'a1b55a9fa815fa90cf817b0390a430cf',  // Ваш api_hash
-                ],
-                'connections' => [
-                    'proxy' => null,
-                    'use_ipv6' => false,
-                ],
-            ]);
+            // $settings = new Settings([
+            //     'app_info' => [
+            //         'api_id' => '23309931',  // Ваш api_id
+            //         'api_hash' => 'a1b55a9fa815fa90cf817b0390a430cf',  // Ваш api_hash
+            //     ],
+            //     'connections' => [
+            //         'proxy' => null,
+            //         'use_ipv6' => false,
+            //     ],
+            // ]);
             
             $MadelineProto = new API($this->sessionFile, $settings);
 
