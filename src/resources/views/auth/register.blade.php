914aa544427102357e191c6442d268f6b2eb547d
ADIPISKAR <pazin-artem@mail.ru>
2025-01-03 12:52:29 +0300

    1

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -1,11 +1,76 @@
 @extends('layouts.guest')
 
 @section('content')
-<div class="container">
+    <div class="row justify-content-center h-100 align-content-center flex-column">
+        <div class="col-md-8 col-md-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4">
+
+            <div class="d-flex flex-column align-items-start mb-3">
+                <p class="text-center Main-Text">Регистрация в SendDive</p>
+            </div>
+
+            <form action="{{ route('register') }}" method="POST">
+                @csrf
+
+                <div class="mb-3">
+                    <p class="Second-text mb-1">ФИО</p>
+                    <input id="FIO" type="text" class="form-input" name="email" value="{{ old('email') }}" required autocomplete="off" autofocus>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <p class="Second-text mb-1">Емейл</p>
+                    <input id="email" type="email" class="form-input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="off" autofocus>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <p class="Second-text mb-1">Телефон</p>
+                    <input id="tel" type="tel" class="form-input" name="email" value="{{ old('email') }}" required autocomplete="off" placeholder="8 (___) ___-__-__" autofocus>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+
+                <button class="w-100 btn btn-primary form-button">Зарегистрироваться</button>
+            </form>
+
+        </div>
+
+        <div class="col-md-8 col-md-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4">
+            <form action="{{ route('register') }}" method="POST">
+                @csrf
+                <div class="d-flex flex-row align-items-baseline">
+                    <div class="line"></div>
+                    <p class="Second-text m-0 mx-2">или</p>
+                    <div class="line"></div>
+                </div>
+
+                <div class="d-flex justify-content-center mt-3">
+                    <p class="Second-text m-0 mx-2">Уже есть аккаунт?<a href="/login" class="Second-text-link mx-1">Войдите</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
+
+
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">{{('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -73,5 +138,4 @@
             </div>
         </div>
     </div>
-</div>
-@endsection
+</div> --}}
