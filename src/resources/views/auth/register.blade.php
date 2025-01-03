107381ff8b7716687b9dc9a60e871e944ed805af
ADIPISKAR <pazin-artem@mail.ru>
2025-01-03 13:41:28 +0300

    Update register.blade.php

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@
 
                 <div class="mb-3">
                     <p class="Second-text mb-1">ФИО</p>
-                    <input id="name" type="text" class="form-input" name="name" required autocomplete="off" autofocus>
+                    <input id="name" type="text" class="form-input" name="name"  required autocomplete="off" autofocus>
                     @error('email')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
